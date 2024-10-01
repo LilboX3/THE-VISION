f361108d5a9fa5a323d91b6dd8a51ec511dfc8c8
LilboX3 <lilligruber02@gmail.com>
2024-10-01 21:32:51 +0200

    Added elemental logic

---
+++ ACTUAL dungeon crawler/Assets/Script/Element.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using static Elements;
+
+public class Element : MonoBehaviour
+{
+    public ElementType elementType { get; set; }
+    public enum ElementType
+    {
+        Melee,
+        Magic,
+        Sin,
+        Prayer
+    }
+
+    public double getDamageModifierAttackedBy(Element otherElement)
+    {
+        switch (this.elementType)
+        {
+            case ElementType.Melee:
+                if (otherElement.elementType == ElementType.Prayer)
+                    return 2;
+                if (otherElement.elementType == ElementType.Magic)
+                    return 1 / 2;
+                break;
+
+            case ElementType.Magic:
+                if (otherElement.elementType == ElementType.Melee)
+                    return 2;
+                if (otherElement.elementType == ElementType.Sin)
+                    return 1 / 2;
+                break;
+
+            case ElementType.Sin:
+                if (otherElement.elementType == ElementType.Magic)
+                    return 2;
+                if (otherElement.elementType == ElementType.Prayer)
+                    return 1 / 2;
+                break;
+
+            case ElementType.Prayer:
+                if (otherElement.elementType == ElementType.Sin)
+                    return 2;
+                if (otherElement.elementType == ElementType.Melee)
+                    return 1 / 2;
+                break;
+        }
+
+        return 1;
+    }
+}
